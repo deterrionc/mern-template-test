6cb7a0bee3b83fd4d5e005f524aa2cdfcd8a3712
daskiro514 <luckybuzstar@gmail.com>
2022-07-25 22:42:13 +0900

    procfile

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+} else {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 const PORT = process.env.PORT || 5001
